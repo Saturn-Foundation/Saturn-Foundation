5494a92770a91b43462d30f18794dcde84bac97d
Complexia <complexia701@gmail.com>
2024-09-21 23:04:30 +0800

    lz stuff

---
+++ saturn-blocks/script/DeployOptimismVerifier.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/OptimismVerifier.sol";
+
+contract DeployOptimismVerifier is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address lzEndpoint = vm.envAddress("LZ_ENDPOINT");
+        address worldIdAddress = vm.envAddress("WORLD_ID_ADDRESS");
+        string memory appId = vm.envString("APP_ID");
+        string memory actionId = vm.envString("ACTION_ID");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        OptimismVerifier verifier = new OptimismVerifier(
+            lzEndpoint,
+            IWorldID(worldIdAddress),
+            appId,
+            actionId
+        );
+
+        console.log("OptimismVerifier deployed at:", address(verifier));
+
+        vm.stopBroadcast();
+    }
+}
--- saturn-blocks/script/DeploySaturnToken.s.sol
+++ saturn-blocks/script/DeploySaturnToken.s.sol
@@ -3,33 +3,20 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "../src/SaturnToken.sol";
-import {IWorldID} from "../src/interfaces/IWorldID.sol";
 
 contract DeploySaturnToken is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address worldIdAddress = vm.envAddress("WORLD_ID_ADDRESS");
-        
-        string memory appId = vm.envString("WORLD_ID_APP_ID");
-        string memory actionId = vm.envString("WORLD_ID_ACTION_ID");
+        address lzEndpoint = vm.envAddress("LZ_ENDPOINT");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        uint256 frequency = 1 days;
-        uint256 amount = 1 ether;
-        IWorldID worldId = IWorldID(worldIdAddress);
-        //IWorldID worldId;
-        
-        SaturnToken saturnToken = new SaturnToken(
-            frequency,
-            amount,
-            worldId,
-            appId,
-            actionId
+        SaturnToken token = new SaturnToken(
+            lzEndpoint
         );
 
-        vm.stopBroadcast();
+        console.log("SaturnToken deployed at:", address(token));
 
-        console.log("SaturnToken deployed at:", address(saturnToken));
+        vm.stopBroadcast();
     }
 }
